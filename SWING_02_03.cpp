d39d4c4178f65793d9e60edfa3e981b440bfa5c5
Yheeseo <81593361+Yheeseo@users.noreply.github.com>
2021-04-27 19:38:22 +0900

    Add files via upload

---
+++ SWING_02_03.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std; 
+
+int main()
+{
+	int a = 10; // a 선언
+	int* ptr; // 포인터 선언
+	ptr = &a; // ptr에 a의 주소 대입
+
+	cout << "변수 a의 주소는 : " << ptr << endl;
+	cout << "a의 값은 : " << a << endl;
+	return 0;
+}
